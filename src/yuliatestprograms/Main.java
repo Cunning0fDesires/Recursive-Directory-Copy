ba5c9b23ea764b9f03dbb7d308e6e9e64f80f51e
Yulchik <ysosnina1@gmail.com>
2021-01-18 14:17:38 +0300

    Implemented the copyFile method

--- src/yuliatestprograms/Main.java
+++ src/yuliatestprograms/Main.java
@@ -1,6 +1,6 @@
 package yuliatestprograms;
 
-import java.io.File;
+import java.io.*;
 
 public class Main {
 
@@ -8,15 +8,30 @@ public class Main {
 
     }
 
-    public static void copyDirectory (String source, String destination){
+    public static void copyDirectory(String source, String destination) {
         File sourceDirectory = new File(source);
         File destDirectory = new File(destination);
 
     }
 
 
-    public static void copyFile (File source, File destination) {
+    public static void copyFile(File source, File destination) {
+        try (InputStream sourceStream = new FileInputStream(source);
+             OutputStream destStream = new FileOutputStream(destination);
+        ) {
+            byte[] buffer = new byte[1024];
+            int count = 0;
+            while ((count = sourceStream.read(buffer)) > 0) {
+                destStream.write(buffer, 0, count);
+            }
+            System.out.println("File " + source.getAbsolutePath()
+                    + " was copied to " + destination.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
-
 }
+
