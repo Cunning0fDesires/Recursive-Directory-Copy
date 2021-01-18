9f9d6e1c8fff83e18c9b353d97775fa9c754485f
Yulchik <ysosnina1@gmail.com>
2021-01-18 13:54:40 +0300

    Added the signature of the copyDirectory and copyFile methods

--- src/yuliatestprograms/Main.java
+++ src/yuliatestprograms/Main.java
@@ -1,8 +1,22 @@
 package yuliatestprograms;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
+
+    public static void copyDirectory (String source, String destination){
+        File sourceDirectory = new File(source);
+        File destDirectory = new File(destination);
+
+    }
+
+
+    public static void copyFile (File source, File destination) {
+
+    }
+
 }
